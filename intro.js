53e0a94ca94ef0371bd5ef91948b8aa562f7411f
Jon Corbett <corbett.jo@husky.neu.edu>
2016-10-27 21:51:20 -0400

    Mobile Optimization part 3

---
+++ intro.js
@@ -0,0 +1,36 @@
+// click "Run" to replay
+
+var $body = $(".landing");
+
+var loading = [
+	{
+		elements: $body,
+		properties: {
+			height: '0%',
+			width: '0%'
+		}
+
+
+	},
+	{
+		elements: $body,
+		properties: {
+			width: '0%'
+		}
+	},
+	{
+		elements: $body,
+		properties: {
+			height: '50%',
+			width: '30%'
+		},
+		options: {
+			complete: function () {
+				$('.wrap').velocity('transition.slideUpIn');
+				$('img').velocity('transition.flipYIn');
+			}
+		}
+  }
+];
+
+$.Velocity.RunSequence(loading);

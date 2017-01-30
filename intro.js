bbce5c829f6179fbedf769946210f2c164bc4acf
Jon Corbett <corbett.jo@husky.neu.edu>
2017-01-29 20:52:47 -0500

    home page mobile bug fix

--- intro.js
+++ intro.js
@@ -1,43 +1,43 @@
-var body = $(".landingBackground");
+if (screen.width > 480) {
 
-var landWidth = $(".homenav").width() + 80;
-var landHeight = $(".homenav").height() + 20;
+	var body = $(".landingBackground");
+	var landWidth = $(".landingBackground").width() + 80;
+	var landHeight = $(".landingBackground").height() + 20;
 
-
-
-var loading = [
-	{
-		elements: body,
-		properties: {
-			height: '10%'
-		}
-	},
-	{
-		elements: body,
-		properties: {
-			hight: '10%',
-			width: landWidth
-		}
-	},
-	{
-		elements: body,
-		properties: {
-			height: '50%'
+	var loading = [
+		{
+			elements: body,
+			properties: {
+				height: '10%'
+			}
 		},
-		options: {
-			complete: function () {
-				{
-					$(".landing").fadeIn();
-
-
-
+		{
+			elements: body,
+			properties: {
+				hight: '10%',
+				width: landWidth
+			}
+		},
+		{
+			elements: body,
+			properties: {
+				height: '50%'
+			},
+			options: {
+				complete: function () {
+					{
+						$(".landing").fadeIn();
+					}
 				}
 			}
 		}
-	}
-
-];
+	];
 
 
-$(".landing").hide();
-$.Velocity.RunSequence(loading);
+	$(".landing").hide();
+	$.Velocity.RunSequence(loading);
+
+} else {
+	$(".landingBackground").width(400);
+	$(".landingBackground").height(350);
+}
